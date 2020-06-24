42f966175c723332489ee0d9c99d12631f070eb2
Hae-Ji Park <positive235@gmail.com>
2020-06-24 15:48:56 -0700

    notify the winner

--- src/index.js
+++ src/index.js
@@ -38,8 +38,14 @@ class Board extends React.Component {
   }
   
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); 
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -85,4 +91,26 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;  
+}
+
+
   
