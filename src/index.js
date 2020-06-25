4f3a750a676c7ee9d5faae3e8e4b5f94cac1d4be
Hae-Ji Park <positive235@gmail.com>
2020-06-24 17:12:24 -0700

    tutorial end

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       stepNumber: 0,
-      xIsNext: true,
+      xIsNext: true
     };
   }
 
@@ -109,12 +109,12 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick={i => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-        <div>{status}</div>
-        <ol>{moves}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
